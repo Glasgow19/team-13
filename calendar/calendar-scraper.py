34bea9a0ccaa54e13355422e2b78af389368ebf2
olliegardner <olliegardner@gmail.com>
2019-10-26 20:44:41 +0100

    refactored class

--- calendar/calendar-scraper.py
+++ calendar/calendar-scraper.py
@@ -3,6 +3,7 @@ import datetime
 import dateutil.parser
 import pickle
 import os.path
+from datetime import timedelta
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -10,7 +11,7 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 appointments = []
-hours = (datetime.datetime(2019, 10, 28, 6), datetime.datetime(2019, 10, 28, 22))
+hours = (datetime.datetime(2019, 10, 28, 6), datetime.datetime(2019, 10, 28, 22)) # the hours of the day at which appointments can be made
 
 def main():
     creds = None
@@ -37,18 +38,14 @@ def main():
     events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=30, singleEvents=True, orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-
     if events:
-        #print(events)
-
         for event in events:
-            ##print(event['start'])
+            # this is horrible, needs re-written
             start = event['start'].get('dateTime', event['start'].get('date'))
             end = event['end'].get('dateTime', event['end'].get('date'))
 
             start_parsed = str(dateutil.parser.parse(start))
             end_parsed = str(dateutil.parser.parse(end))
-            #print(parsedDate, event['summary'])
 
             start_parsed_split = start_parsed.split(' ')
             end_parsed_split = end_parsed.split(' ')
@@ -66,38 +63,16 @@ def main():
             end_hour = end_time_split[0]
             end_mins = end_time_split[1]
 
+            # adds appointment in list to datetime format
             appointments.append((datetime.datetime(int(year), int(month), int(day), int(start_hour), int(start_mins)), datetime.datetime(int(year), int(month), int(day), int(end_hour), int(end_mins))))
 
-    
-    '''
-    import pytz
-    tz = pytz.timezone('UTC')
-    
-    the_datetime = tz.localize(datetime.datetime(2019, 10, 28, 0))
-    the_datetime2 = tz.localize(datetime.datetime(2019, 10, 31, 8))
-    body = {
-        "timeMin": the_datetime.isoformat(),
-        "timeMax": the_datetime2.isoformat(),
-        "timeZone": 'UTC',
-        "items": [{"id": 'primary'}]
-       }
-    eventsResult = service.freebusy().query(body=body).execute()
-    print(eventsResult)
-    '''
 
-from datetime import timedelta
-
-'''appointments = [(datetime.datetime(2012, 5, 22, 10), datetime.datetime(2012, 5, 22, 10, 30)),
-                (datetime.datetime(2012, 5, 22, 12), datetime.datetime(2012, 5, 22, 13)),
-                (datetime.datetime(2012, 5, 22, 15, 30), datetime.datetime(2012, 5, 22, 17, 10))]'''
-
-
+# gets free slots in the user's calendar
 def get_slots(hours, appointments, duration=timedelta(hours=1)):
-    print(appointments)
     slots = sorted([(hours[0], hours[0])] + appointments + [(hours[1], hours[1])])
-    for start, end in ((slots[i][1], slots[i+1][0]) for i in range(len(slots)-1)):
-        assert start <= end, "Cannot attend all appointments"
-        while start + duration <= end:
+
+    for start, end in ((slots[i][1], slots[i + 1][0]) for i in range(len(slots) - 1)):
+        while (start + duration) <= end:
             print("{:%H:%M} - {:%H:%M}".format(start, start + duration))
             start += duration    
 
